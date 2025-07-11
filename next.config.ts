dd23c8ad2d8a21356a4f2dbdca7e82432d96e551
hbang012 <hbang4888@gmail.com>
2025-07-11 16:08:36 +0900

    fix: next.config 수정2
    ```

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
+// next.config.ts
 import type { NextConfig } from 'next';
 
+// 외부 url을 사용한 데이터 이미지일 경우 악의적인 사용자로 부터 보호하기위해
+// 특정 도메인의 이미지만 사용가능하도록 설정
 const nextConfig: NextConfig = {
   images: {
-    domains: ['api.hbjaws.com'],
-    // 또는 remotePatterns 로 더 세밀하게:
     remotePatterns: [
       {
         protocol: 'https',
