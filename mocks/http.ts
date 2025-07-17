b65233188fa75a907ad742da6f4d580ada0c6aee
hbang012 <hbang4888@gmail.com>
2025-07-17 17:52:37 +0900

    fix: 검색 기능 수정 1
    ```

--- mocks/http.ts
+++ mocks/http.ts
@@ -1,8 +1,7 @@
 // 여기서 만든 핸들러 연결
-import { createMiddleware } from '@mswjs/http-middleware';
+// import { createMiddleware } from '@mswjs/http-middleware';
 import express from 'express';
 import cors from 'cors';
-import { articlesHandlers } from '@/mocks/articles-handlers';
 
 const app = express();
 const port = 9090;
@@ -15,7 +14,7 @@ app.use(
   })
 );
 app.use(express.json());
-app.use(createMiddleware(...articlesHandlers));
+// app.use(createMiddleware(...articlesHandlers));
 // NSW핸들러연결
 
 app.listen(port, () => console.log(`Mock server is running on port: ${port}`));
